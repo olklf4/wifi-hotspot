aa55019b5d8e2f7367e80723df799672737dbcd7
olklf4 <6MEg6B+olklf4@gmail.com>
2024-11-18 18:45:01 +0700

    Add readme, license files and Windows Console Application template
    License: CC BY-NC-ND 4.0
    The main script shows the license name

---
+++ README.md
@@ -0,0 +1,3 @@
+## WiFi Hotspot
+C++/WinRT
+###### WiFi Hotspot © 2024 by Kevin is licensed under [Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International](https://creativecommons.org/licenses/by-nc-nd/4.0/)
---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace std;
+
+int main()
+{
+	init_apartment();
+	cout << "WiFi Hotspot © 2024 by Kevin is licensed under CC BY-NC-ND 4.0\n";
+	uninit_apartment();
+}
